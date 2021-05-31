e40567c093956a24c505500206fc82710fdbf45b
Tinku Bansal <tinkubansal21@gmail.com>
2021-05-31 18:30:49 +1000

    Edited ReadMe file

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,5 @@
 const path = require("path");
-require('dotenv').config({path : path.join(__dirname,"../.env.Example")});
+require('dotenv').config({path : path.join(__dirname,"../.env")});
 
 const Sequelize = require('sequelize');
 
